0e85a8ea36b9577363ee2125630b7fc20220eda0
Munir-Rahman <munirrahmanrahmani0@gmail.com>
2025-07-26 23:02:15 +0430

    Add Tic Tac Toe Game Code Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tic-Tac-Toe-Game
 A simple and interactive Tic Tac Toe game built using HTML, CSS and JavaScript. This project demonstrate the core concepts of front-end development, including DOM manipulation, event handling, and game logic 
+Author -Munir Rahman Rahmani
---
+++ js/main.js
@@ -0,0 +1,105 @@
+let boxes = document.querySelectorAll(".box");
+let msgContainer = document.querySelector(".hide");
+let resetbtn = document.getElementById("rst-btn");
+let newgamebtn = document.getElementById("new-btn");
+let message = document.getElementById("message");
+let turnX = true; //PLayer X and Player O
+let count = 0; //To Find Game Draw
+
+const patterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 3, 6],
+    [2, 4, 6]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnX && !box.innerText){
+            box.innerText = "X";
+            turnX = false;
+        }else if(!turnX && !box.innerText){
+            box.innerText = "O";
+            turnX = true;
+        }
+        let iswinner = checkWinner();
+        count++;
+        if(count === 9 && !iswinner){
+            msgContainer.classList.remove("hide");
+            gameDraw();
+        } 
+    });
+});
+
+//GameDraw
+let gameDraw = () => {
+    message.innerText = "Game Was Draw";
+    disablebtns();
+}
+
+//Disable Buttons
+let disablebtns = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+//Enable Buttons
+let enablebuttons = () => {
+    for(let box of boxes){
+        box.disabled = false;
+    }
+}
+
+//Reset Game and New Game
+let resetgame = () => {
+    for(let box of boxes){
+        box.innerText = "";
+    }
+    turnO = true;
+    count = 0;
+    enablebuttons();
+    msgContainer.classList.add("hide");
+}
+
+//New Game
+let newgame = () => {
+    for(let box of boxes){
+        box.innerText = "";
+    }
+    turnO = true;
+    count = 0;
+    enablebuttons();
+    msgContainer.classList.add("hide");
+}
+
+//Show Winner
+let showWinner = (winner) => {
+    message.innerText = `Congratulations Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disablebtns();
+}
+
+//Check Winner
+const checkWinner = () => {
+    for(pettern of patterns){
+        let value1 = boxes[pettern[0]].innerText;
+        let value2 = boxes[pettern[1]].innerText;
+        let value3 = boxes[pettern[2]].innerText;
+        
+        if(value1 !== "" && value2 !== "" && value3 !== ""){
+            if(value1 === value2 && value2 === value3){
+                showWinner(value1);
+                return true;
+            }
+        }
+    }
+} 
+
+//Reset and New Buttons
+resetbtn.addEventListener("click",resetgame);
+newgamebtn.addEventListener("click", newgame);
